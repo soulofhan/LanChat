780550295f5d18521dddfda4520878756f68e91a
不死焱炎 <hgj19950619@gmail.com>
2018-10-24 18:17:26 +0800

    1、连接请求 2、服务器公告

--- Server.py
+++ Server.py
@@ -32,11 +32,9 @@ def tellMySelf(exceptNum, whatToSay):
                 print('32 exception is : %s' % e)
 
 
-# 向所有人发送当前在线人员昵称
-def tellAll():
-    mList = list(myDict.values())
-    print('38 当前在线人员为:%s' % mList)
-    whatToSend = '[Tip]:%s' % mList
+# 向所有人发送服务器公告
+def notificationAll(bulletin):
+    whatToSend = '[proclamation]:[%s]' % bulletin
     for allClient in myList:
         try:
             allClient.send((whatToSend + '\n').encode())
@@ -46,7 +44,7 @@ def tellAll():
 
 def isName(nickName):
     nameList = list(myDict.values())
-    if nickName not in nameList:
+    if nickName not in nameList and nickName.split():
         print('50 传入的值为 %s 列表为 %s' % (nickName, nameList))
         return True  # 不在列表中返回True
     return False
@@ -65,44 +63,54 @@ def sendAll():
 
 def subThreadIn(myConnection, connNumber):
     nickname = ''
+    disconnect = False
+    mContinue = True
     while True:
         try:
             nickname = myConnection.recv(1024).decode()
         except IOError as mIOError:
             print('72 recv exceptional %s' % mIOError)
-        if isName(nickname):
-            myDict[connNumber] = nickname  # 将初始化昵称加入至在线人列表
-            myList.append(myConnection)  # 将连接加入在线客户端列表
-            myConnection.send('[correct]:[success]\n'.encode())  # 发送链接成功
+        if nickname == 'disconnect':
+            myConnection.send('disconnect\n'.encode())  # 发送终止链接指令
+            mContinue = False
+            myConnection.close()
+            print('79 close the connection %s' % myConnection)
             break
         else:
-            myConnection.send('[correct]:[failure]\n'.encode())  # 发送链接成功
-            break
-    print('81 connection', connNumber, ' has nickname :', nickname)
-    # 向其他人发送自己加入房间
-    tellOthers(connNumber, '[enter]:[' + myDict[connNumber] + ']')
-    # 向自己发送当前在线人员
-    tellMySelf(connNumber, '[Tip]:%s' % list(myDict.values()))
-    sendAll()
-    disconnect = False
-    while True:
-        if disconnect:
-            tellMySelf(connNumber, 'disconnect')  # 向自己发送断开连接指令
-            leave(myConnection, connNumber)  # 告诉其他人我已离开
-            sendAll()
-            return
-        else:
-            try:
-                recvedMsg = myConnection.recv(1024).decode()  # 阻塞接收消息
-                if recvedMsg == 'disconnect' or not recvedMsg.strip():  # 如果收到'disconnect' 则将退出位置True
-                    disconnect = True
-                else:
-                    print('100', myDict[connNumber], ':', recvedMsg)  # 输出接收到的消息
-                    tellOthers(connNumber, '[Msg]:[' + myDict[connNumber] + ',' + recvedMsg + ']')
+            if isName(nickname):
+                myDict[connNumber] = nickname  # 将初始化昵称加入至在线人列表
+                myList.append(myConnection)  # 将连接加入在线客户端列表
+                myConnection.send('[correct]:[success]\n'.encode())  # 发送链接成功
+                break
+            else:
+                myConnection.send('[correct]:[failure]\n'.encode())  # 发送链接失败
+                continue
 
-            except (OSError, ConnectionResetError):
-                leave(myConnection, connNumber)  # 客户端直接退出时执行异常连接断开
+    if mContinue:
+        print('81 connection', connNumber, ' has nickname :', nickname)
+        # 向其他人发送自己加入房间
+        tellOthers(connNumber, '[enter]:[' + myDict[connNumber] + ']')
+        # 向自己发送当前在线人员
+        tellMySelf(connNumber, '[Tip]:%s' % list(myDict.values()))
+        sendAll()
+        while True:
+            if disconnect:
+                tellMySelf(connNumber, 'disconnect')  # 向自己发送断开连接指令
+                leave(myConnection, connNumber)  # 告诉其他人我已离开
+                sendAll()
                 return
+            else:
+                try:
+                    recvedMsg = myConnection.recv(1024).decode()  # 阻塞接收消息
+                    if recvedMsg == 'disconnect' or not recvedMsg.strip():  # 如果收到'disconnect' 则将退出位置True
+                        disconnect = True
+                    else:
+                        print('100', myDict[connNumber], ':', recvedMsg)  # 输出接收到的消息
+                        tellOthers(connNumber, '[Msg]:[' + myDict[connNumber] + ',' + recvedMsg + ']')
+
+                except (OSError, ConnectionResetError):
+                    leave(myConnection, connNumber)  # 客户端直接退出时执行异常连接断开
+                    return
 
 
 # 离开函数
@@ -117,6 +125,17 @@ def leave(myConnection, connNumber):
     myConnection.close()  # 关闭连接
 
 
+def notification():
+    while True:
+        notificationMsg = input()
+        notificationAll(notificationMsg)
+
+
+# 启动一个系统通知线程
+sendThread = threading.Thread(target=notification)
+sendThread.start()
+
+# 循环等待客户端接入
 while True:
     connection, addr = sock.accept()  # 阻塞接入客户端
     print('122 Accept a new connection', connection, connection.getsockname(), connection.fileno(), addr)
@@ -124,7 +143,7 @@ while True:
         # connection.settimeout(5)
         buf = connection.recv(1024).decode()
         if buf == '1':
-            connection.send('welcome to server!\n'.encode())
+            connection.send('[wel]:[welcome]\n'.encode())
             connection.send(('[decide]:[' + str(len(myList)) + ']\n').encode())
 
             # 为当前连接开辟一个新的线程
